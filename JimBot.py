44602b02ea7f0212b2cff83f21db215adc7aff37
jamesw86 <chokkore86@gmail.com>
2020-08-04 23:31:56 -0400

    Converging JimBot

---
+++ JimBot.py
@@ -0,0 +1,28 @@
+import discord
+import image
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+    await client.change_presence(activity=discord.Game(name='with numbers'))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+    if "COVID" in message.content:
+        days = image.getDays()
+        try:
+            f = discord.File("Day" + str(days) + ".jpg")
+            await message.channel.send(file=f)
+        except Exception as err:
+            await message.channel.send("Error! Please contact Jim.")
+            print(err)
+    
+
+client.run(process.env.BOT_TOKEN)
---
+++ image.py
@@ -0,0 +1,24 @@
+from PIL import Image, ImageFont, ImageDraw, ImageOps
+import datetime
+import tempfile
+from datetime import timedelta
+
+def getDays():
+    td = datetime.datetime.now() - datetime.datetime(2020, 4, 1) 
+    days = td.days
+    return days
+
+
+date_text = "Day " + str(getDays()) + " of lockdown:"
+
+im = Image.open('egg_offer.jpg')
+im = ImageOps.pad(im, (im.size[0],im.size[1]+50), color='white', centering=(0.5,1))
+draw = ImageDraw.Draw(im)
+
+# use a truetype font
+font = ImageFont.truetype("arial.ttf", 30)
+
+draw.text((10, 15), date_text, font=font, fill="black")
+
+im.save("Day" + str(getDays()) + ".jpg")
+
