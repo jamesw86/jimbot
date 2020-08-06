4dad4e28254047c4a6b533939faa068b9531f520
jamesw86 <chokkore86@gmail.com>
2020-08-06 16:39:59 -0400

    Testing out postgresql

--- TestScript.py
+++ TestScript.py
@@ -1,2 +1,8 @@
-import discord
-print("Hello World!")
+import os
+import psycopg2
+try:
+    DATABASE_URL = os.environ['DATABASE_URL']
+    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+    print("Connected!")
+except:
+    print("Error connecting.")
